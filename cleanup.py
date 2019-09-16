88c2001be5f21ade05cee4789535c35a53340d79
LordMathis <matus@namesny.com>
2019-09-16 22:23:39 +0200

    Port to python 3

--- cleanup.py
+++ cleanup.py
@@ -13,15 +13,16 @@ apiKey = argv[3]
 secret = argv[4]
 
 method = "GET"
-path = "/v1/user/self/zone/" + certbot_domain + "/record"
+path = "/v1/user/self/zone/{}/record".format(certbot_domain)
 timestamp = int(time.time())
 api = "https://rest.websupport.sk"
 
-canonicalRequest = "%s %s %s" % (method, path, timestamp)
+canonicalRequest = "{} {} {}".format(method, path, timestamp)
 signature = hmac.new(secret.encode('utf-8'), canonicalRequest.encode('utf-8'), hashlib.sha1).hexdigest()
+credentials = base64.b64encode("{}:{}".format(apiKey, signature).encode('utf-8'))
 
 headers = {
-    "Authorization": "Basic %s" % (base64.b64encode("{}:{}".format(apiKey, signature).encode('utf-8'))),
+    "Authorization": "Basic {}".format((credentials).decode('utf-8')),
     "Content-Type": "application/json",
     "Accept": "application/json",
     "Date": datetime.fromtimestamp(timestamp).isoformat()
@@ -31,7 +32,7 @@ res_dict = json.loads(certbot_auth_output)
 record_id = res_dict['item']['id']
 
 # This GET request is necessary, otherwise you'll get 403 response on the DELETE request
-requests.get("%s%s" % (api, path), headers=headers).content
+requests.get("%s%s" % (api, path), headers=headers)
 
 path = path + "/" + str(record_id)
-print requests.delete("%s%s" % (api, path), headers=headers).content
+print(requests.delete("%s%s" % (api, path), headers=headers).text)
